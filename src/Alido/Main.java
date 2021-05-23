307d2a6a4f52530a40aac06bed2112420fe8cdcb
swartsd <swa17001@byui.edu>
2021-05-22 21:17:42 -0600

    Added my name

--- src/Alido/Main.java
+++ src/Alido/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Frances");
+        System.out.println("Hello Frances, my name is Dallin");
     }
 }
