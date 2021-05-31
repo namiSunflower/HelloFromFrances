6f728852fc9d807d5ff155502e73621205176ddd
KANe <kan21002@byui.edu>
2021-05-31 11:00:58 +0200

    mike changes

--- src/Alido/Main.java
+++ src/Alido/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello, my name is Frances");
         System.out.println("Hello Frances, my name is Dallin");
+        System.out.println("Hello Frances, my name is Michael");
     }
 }
